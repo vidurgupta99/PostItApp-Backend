a67c2319fd65a9eb6f9bb73690c6493bd644abe4
vidurgupta99 <123901820+vidurgupta99@users.noreply.github.com>
2023-04-22 14:16:32 -0400

    Updated Utils.py

--- api/utils.py
+++ api/utils.py
@@ -1,3 +1,42 @@
 from rest_framework.response import Response
-from .models import Note
+from .models import PostIt
 from .serializers import PostItSerializer
+
+# API Routes
+
+# Create
+def createPostIt(request):
+    serializer = PostItSerializer(postit, many=False)
+    data = request.data
+    postit = PostIt.objects.create(
+        body=data['body']
+    )
+    return Response(serializer.data)
+
+# Read
+def getPostIts(request):
+    serializer = PostItSerializer(postits, many=True)
+    postits = PostIt.objects.all().order_by('-updated')
+    return Response(serializer.data)
+
+
+def getPostItInfo(request, pk):
+    serializer = PostItSerializer(postits, many=False)
+    postits = PostIt.objects.get(id=pk)
+    return Response(serializer.data)
+
+# Update
+def updatePostIt(request, pk):
+    serializer = PostItSerializer(instance=postit, data=data)
+    data = request.data
+    postit = PostIt.objects.get(id=pk)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return serializer.data
+
+# Delete
+def deletePostIt(request, pk):
+    postit = PostIt.objects.get(id=pk)
+    postit.delete()
--- api/views.py
+++ api/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.serializers import Serializer
+from .models import PostIt
+from .serializers import PostItSerializer
