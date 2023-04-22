4ff5ef4f272a30c751e85dd291e10b883da2f24d
vidurgupta99 <123901820+vidurgupta99@users.noreply.github.com>
2023-04-22 14:51:57 -0400

    Updated Views and Urls

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('postits/', views.getPostIts, name="postits"),
-    path('postits/<str:pk>/', views.getPostIts, name="postit"),
-]
+    path('postits/<str:pk>/', views.getPostIt, name="postit"),
--- api/views.py
+++ api/views.py
@@ -4,4 +4,65 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.serializers import Serializer
 from .models import PostIt
+from api import serializers
 from .serializers import PostItSerializer
+from .utils import updatePostIt, getPostItInfo, deletePostIt, getPostIts, createPostIt
+# Create your views here.
+
+
+@api_view(['GET'])
+def getRoutes(request):
+
+    routes = [
+       
+        {
+            'Endpoint': '/postits/',
+            'method': 'GET',
+            'body': None,
+        },
+        {
+            'Endpoint': '/postits/id',
+            'method': 'GET',
+            'body': None,
+        },
+        {
+            'Endpoint': '/postits/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+        },
+        {
+            'Endpoint': '/postits/id/update/',
+            'method': 'PUT',
+            'body': {'body': ""},
+        },
+        {
+            'Endpoint': '/postits/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+        },
+    ]
+    return Response(routes)
+
+# Get, Put, and Delete Routes
+@api_view(['GET', 'PUT', 'DELETE'])
+def getPostIt(request, pk):
+
+    if request.method == 'GET':
+        return getPostItInfo(request, pk)
+
+    if request.method == 'PUT':
+        return updatePostIt(request, pk)
+
+    if request.method == 'DELETE':
+        return deletePostIt(request, pk)
+        
+# Get and Post Routes
+@api_view(['GET', 'POST'])
+def getPostIts(request):
+
+    if request.method == 'GET':
+        return getPostIts(request)
+
+    if request.method == 'POST':
+        return createPostIt(request)
+
