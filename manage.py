4a223533529bc1a48c749f0301f2db6b62bf1624
vidurgupta99 <123901820+vidurgupta99@users.noreply.github.com>
2023-04-24 15:33:38 -0400

    Added build

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mynotes.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PostItNotes.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
